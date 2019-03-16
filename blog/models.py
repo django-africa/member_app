15a3815b136a53c9e6b133203ab89a1801b22846
Dukesanmi <42076983+Dukesanmi@users.noreply.github.com>
2019-03-16 16:54:48 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+	post_title=models.CharField(max_length=250)
+	post_author=models.ForeignKey(auth.User, on_delete=models.CASCADE)
+	published_date=models.DateTimeField(auto_now_add=True)
+	post_body=models.TextField()
+	image=models.FileField()
+
+	def __str__(self):
+		return self.post_title + '_' + self.post_author
