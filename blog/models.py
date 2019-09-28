8b491193462916361a0f7f8597a1fca5928ea11d
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2019-09-28 17:04:48 +0100

    Added the requirements.txt file

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from django.contrib.auth import models as model
 # Create your models here.
 
 class Posts(models.Model):
-	post_title=models.CharField(max_length=250)
-	post_author=models.ForeignKey(auth.User, on_delete=models.CASCADE)
+	post_title = models.CharField(max_length=250)
+	post_author = models.ForeignKey(model.User, on_delete=models.CASCADE)
 	published_date=models.DateTimeField(auto_now_add=True)
 	post_body=models.TextField()
 	image=models.FileField()
