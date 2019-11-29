d0c3d7aad00ebafcb3202a090a8736e42f25f2f5
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2019-11-29 10:34:08 +0100

    updated

--- membersystem/settings.py
+++ membersystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2#v)st2zysd21qzd-xy$*8m!3@#rnyi-e((bx6z_(h5=in3-$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
